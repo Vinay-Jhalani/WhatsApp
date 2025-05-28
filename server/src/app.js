f8847e51403c8e853e9665cf5c8f3f6cfbb6ad8f
Vinay Jhalani <VinayJhalani.dev@gmail.com>
2025-05-28 18:15:53 +0530

    Setup helmet, cors, cookie parser, morgan , etc.

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,51 @@
+import dotenv from "dotenv";
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
+//dotenv
+dotenv.config();
+
+//create express app
 const app = express();
+
+//morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+//helmet
+app.use(helmet());
+
+//parse json req url
+app.use(express.json());
+
+//parse json req body
+app.use(express.urlencoded({ extended: true }));
+
+//sanitize request data
+app.use(mongoSanitize());
+
+//enable cookie parser
+app.use(cookieParser());
+
+//enable compression
+app.use(compression());
+
+//file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//cors
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello");
